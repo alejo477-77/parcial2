1e931e120592659dfa02dc142e3dbedc6d7b77e1
alejo477-77 <alejogarnica2@gmail.com>
2025-11-03 10:18:16 -0500

    se agrega otra parte

--- parcial.cpp
+++ parcial.cpp
@@ -1,62 +1,104 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
- const int MAX_CONDUCTORES=10;
- const int MAX_Viajes=10;
 
- double validarDato(string mensaje){
+const int MAX_CONDUCTORES = 10;
+const int MAX_VIAJES = 10;
+
+double validarDato(string mensaje) {
     double valor;
-
-    do{
-        cout<<mensaje;
-        cin>>valor;
-        if(valor<0){
-            cout<<"error el valor no puede ser negativo";
+    do {
+        cout << mensaje;
+        cin >> valor;
+        if (valor < 0) {
+            cout << "error: el valor no puede ser negativo. Intente nuevamente"<<endl;
         }
-    }while (valor<0);
-    return valor; 
- }
+    } while (valor < 0);
+    return valor;
+}
 
- double calcular_total(double distancia[],double costos[],int Num_viaje[]){
-    double total=0;
-    for(int i=0;i < Num_viaje;i++){
-        total+=distancia[i]*costos[i];
+double calcularTotal(double distancia[], double costos[], int numViajes) {
+    double total = 0;
+    for (int i = 0; i < numViajes; i++) {
+        total += distancia[i] * costos[i];
     }
     return total;
- }
+}
 
- double promedio_Distancia (double distancia[],int Num_viaje[]){
-    double suma=0;
-    for (int i =0; i<Num_viaje; i++){
-        suma+=distancia[i];
+double promedioDistancia(double distancia[], int numViajes) {
+    double suma = 0;
+    for (int i = 0; i < numViajes; i++) {
+        suma += distancia[i];
     }
-    return(Num_viaje>0)? suma/Num_viaje:0;
- }
+    return (numViajes > 0) ? suma / numViajes : 0;
+}
 
- int main(){
-    int num_Conductores;
-    cout<<"ingrese la cantidad de conductores"<<endl;
-    cin>>num_Conductores;
-    while(num_Conductores<=0 || num_Conductores>MAX_CONDUCTORES);
-    cout<<"error cantiada de conductores invalida"<<endl;
-    cin>>num_Conductores;
+int main() {
+    int numConductores;
 
+   
+    do {
+        cout << "ingrese la cantidad de conductores (máx. " << MAX_CONDUCTORES << "): ";
+        cin >> numConductores;
+        if (numConductores <= 0 || numConductores > MAX_CONDUCTORES) {
+            cout << "error cantidad de conductores inválida"<<endl;
+        }
+    } while (numConductores <= 0 || numConductores > MAX_CONDUCTORES);
+
+    
     string nombres[MAX_CONDUCTORES];
-    double total[MAX_CONDUCTORES];
-    double promedio[MAX_CONDUCTORES];
+    double totalGanado[MAX_CONDUCTORES];
+    double promedioKm[MAX_CONDUCTORES];
 
-    for(int i=0; i<num_Conductores; i++){
-        cout<<"conductor"<<(i+1)<<endl;
-        cout<<"nombre;";
-        cin>>nombres[i];
+    
+    for (int i = 0; i < numConductores; i++) {
+        cout << "conductor " << (i + 1) <<endl;
+        cout << "nombre: ";
+        cin >> nombres[i];
 
         int numViajes;
-        cout<<"ingresa el numero de viajes";
-        cin>>numViajes;
-        while(numViajes<=0||numViajes>MAX_Viajes){
-            cout<<"cantidad invalida";
-            cin>>numViajes;
+        do {
+            cout << "ingrese el número de viajes (máx. " << MAX_VIAJES << "): ";
+            cin >> numViajes;
+            if (numViajes <= 0 || numViajes > MAX_VIAJES) {
+                cout << "error cantidad de viajes inválida"<<endl;
+            }
+        } while (numViajes <= 0 || numViajes > MAX_VIAJES);
+
+        double distancias[MAX_VIAJES];
+        double costos[MAX_VIAJES];
+
+        // Datos por viaje
+        for (int j = 0; j < numViajes; j++) {
+            cout << "Viaje " << (j + 1) <<endl;
+            distancias[j] = validarDato("Distancia (km): ");
+            costos[j] = validarDato("Costo por km: ");
         }
 
+        // Calcular total ganado y promedio de distancia
+        totalGanado[i] = calcularTotal(distancias, costos, numViajes);
+        promedioKm[i] = promedioDistancia(distancias, numViajes);
     }
- }
+
+    cout << " RESULTADOS"<<endl;
+    cout << "Nombre\t\tTotal Ganado\tPromedio Distancia"<<endl;
+
+
+    double mayorIngreso = 0;
+    string conductorMayor;
+
+    for (int i = 0; i < numConductores; i++) {
+        cout << nombres[i] << "\t\t$" << totalGanado[i]
+             << "\t\t" << promedioKm[i] << " km\n";
+
+        if (totalGanado[i] > mayorIngreso) {
+            mayorIngreso = totalGanado[i];
+            conductorMayor = nombres[i];
+        }
+    }
+
+    cout << "El conductor con mayor ingreso es: " << conductorMayor<<endl;
+         << " con un total de $" << mayorIngreso << endl;
+
+    return 0;
+}
